ffd78686a9322e9f40d9f2f29e93fd7158e4adfe
Nicolas Marini <nmarini44@gmail.com>
2018-06-26 23:21:51 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? = "pending"
+    if valid? && self.status = "pending"
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
